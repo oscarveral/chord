6ab9fe6e77f68b4733dcb6f4f158252579f7f438
Oscar Vera López <oscar.veral@um.es>
2024-04-11 19:30:45 +0200

    Cambiada interfaz de elementos persistentes. Ahora es una interfaz que expone métodos necesario de implementar.

---
+++ src/main/java/umu/tds/chord/dao/Persistent.java
@@ -0,0 +1,36 @@
+package umu.tds.chord.dao;
+
+/**
+ * Clase abstracta que implementan aquellos elementos del modelo que son 
+ * persistentes.
+ */
+public interface Persistent {
+	
+	/**
+	 * Obtiene la id actual.
+	 * 
+	 * @return Id actual del objeto.
+	 */
+	public int getId();
+	
+	/**
+	 * Indica si al objeto se le ha asignado anteriormente alguna id 
+	 * persistente. Es decir, si se ha registrado mediante un DAO.
+	 * 
+	 * @return {@code true} si se le asignó una id en algun momento mediante
+	 * {@link Persistent#registerId(int)}. 
+	 * {@code false} en otro caso.
+	 */
+	public boolean isRegistered();
+	
+	/**
+	 * Establece la id del objeto. Por defecto si el objeto ya estaba registrado
+	 * no se hace nada.
+	 * 
+	 * @param uuid Id que se desea utilizar para el objeto.
+	 */
+	public default void registerId(int uuid) {
+		if (isRegistered())
+			return;
+	}
+}
