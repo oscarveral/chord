eacf1d9e2b93614d173cdbaf963b33bc5c76eee9
Oscar Vera López <oscar.veral@um.es>
2024-04-13 16:14:06 +0200

    Modificada documentación de la interfaz DAO.

--- src/main/java/umu/tds/chord/dao/DAO.java
+++ src/main/java/umu/tds/chord/dao/DAO.java
@@ -9,19 +9,24 @@ import umu.tds.chord.model.Persistent;
 
 /**
  * Interfaz DAO genérica
- * @param <T> Tipo sobre el que se implementará la interfaz DAO. Debe implementar la interfaz {@link Persistent}.
+ * 
+ * @param <T> Tipo sobre el que se implementará la interfaz DAO. Debe 
+ * implementar la interfaz {@link Persistent}.
  */
 public interface DAO<T extends Persistent> {
 	
 	/**
-	 * Separador utilizado en las representaciones de listas de ids de objetos persistentes {@link DAO#persistentsToString(List)}.
+	 * Separador utilizado en las representaciones de listas de ids de objetos 
+	 * persistentes {@link DAO#persistentsToString(List)}.
 	 */
 	public static final String REPRESENTATION_STRING_SEPARATOR = " ";
 	
 	/**
-	 * Dada una lista de objetos persistentes obtiene una cadena de texto con las ids de los objetos de la lista.
+	 * Dada una lista de objetos persistentes obtiene una cadena de texto con 
+	 * las ids de los objetos de la lista.
 	 * 
 	 * @param list Lista de la que se desea obtener la representacion en texto.
+	 * 
 	 * @return Cadena de texto con ids de los objetos persistentes.
 	 */
 	public static <T extends Persistent> String persistentsToString(List<T> list) {
@@ -31,10 +36,15 @@ public interface DAO<T extends Persistent> {
 	}
 	
 	/**
-	 * Obtiene una lista de objetos persistentes a partir de una cadena de texto que contiene la ids de los objetos a recuperar.
+	 * Obtiene una lista de objetos persistentes a partir de una cadena de 
+	 * texto que contiene la ids de los objetos a recuperar.
 	 * 
-	 * @param representation Cadena que indica los objetos que recuperar. La cadena debe haberse generado mediante {@link DAO#persistentsToString(List)}
-	 * @return Lista de objetos recuperados. Si algun id de la cadena de texto no estaba registrado en persistencia o se falla en obtención es ignorado.
+	 * @param representation Cadena que indica los objetos que recuperar. La 
+	 * cadena debe haberse generado mediante 
+	 * {@link DAO#persistentsToString(List)}
+	 * 
+	 * @return Lista de objetos recuperados. Si algun id de la cadena de texto 
+	 * no estaba registrado en persistencia o se falla en obtención es ignorado.
 	 */
 	public default List<T> stringToPersistents(String representation) {
 		return Arrays.stream(representation.split(REPRESENTATION_STRING_SEPARATOR))
@@ -46,12 +56,18 @@ public interface DAO<T extends Persistent> {
 	}
 	
 	/**
-	 * Registra un nuevo objeto persistente y establece la id persistente del objeto proporcionado.
+	 * Registra un nuevo objeto persistente y establece la id persistente del 
+	 * objeto proporcionado.
 	 * 
 	 * @param t Objeto que se desea registrar y que recibirá una id.
-	 * @return {@code true} si el registro es exitoso. {@code false} si t es {@code null}, su id es mayor a 0 (solo se permite registrar objetos sin id positivas) o ya se encontraba registrado.
 	 * 
-	 * @implNote La implementación por defecto proporciona la comprobación de requisitos básicos comunes que, en su mayoría, comprobarían todas las implementaciones de esta interfaz. 
+	 * @return {@code true} si el registro es exitoso. {@code false} si t es 
+	 * {@code null}, su id es mayor a 0 (solo se permite registrar objetos sin 
+	 * id positivas) o ya se encontraba registrado.
+	 * 
+	 * @implNote La implementación por defecto proporciona la comprobación de
+	 *  requisitos básicos comunes que, en su mayoría, comprobarían todas las 
+	 *  implementaciones de esta interfaz. 
 	 */
 	public default boolean register(T t) {
 		
@@ -67,14 +83,17 @@ public interface DAO<T extends Persistent> {
 	 * Obtiene el objeto identificado mediante el id dado.
 	 * 
 	 * @param id Identificador único del objeto que se desea recuperar.
-	 * @return Devuelve el objeto identificado por id en persistencia en el caso de estar registrado.
+	 * 
+	 * @return Devuelve el objeto identificado por id en persistencia en el 
+	 * caso de estar registrado.
 	 */
 	public Optional<T> recover(int uuid);
 	
 	/**
 	 * Obtiene una lista con todos los objetos registrados.
 	 * 
-	 * @return Lista de solo lectura de todos los objetos de este tipo registrados.
+	 * @return Lista de solo lectura de todos los objetos de este tipo 
+	 * registrados.
 	 */
 	public List<T> recoverAll();
 	
@@ -82,9 +101,13 @@ public interface DAO<T extends Persistent> {
 	 * Modifica el objeto registrado para actualizar los datos persistentes.
 	 * 
 	 * @param t Objeto que se desea modificar en persistencia.
-	 * @return {@code true} si la modificación fue exitosa. {@code false} si el objeto es {@code null} o no está registrado.
 	 * 
-	 * @implNote La implementación por defecto proporciona la comprobación de requisitos básicos comunes que, en su mayoría, comprobarían todas las implementaciones de esta interfaz. 
+	 * @return {@code true} si la modificación fue exitosa. {@code false} si el 
+	 * objeto es {@code null} o no está registrado.
+	 * 
+	 * @implNote La implementación por defecto proporciona la comprobación de 
+	 * requisitos básicos comunes que, en su mayoría, comprobarían todas las 
+	 * implementaciones de esta interfaz. 
 	 */
 	public default boolean modify(T t) {
 		
@@ -100,9 +123,13 @@ public interface DAO<T extends Persistent> {
 	 * Elimina una entidad registrada en persistencia.
 	 * 
 	 * @param t Objeto que se desea eliminar de persistencia.
-	 * @return {@code true} si la eliminación fue exitosa. {@code false} si el objeto es {@code null} o no está registrado.
 	 * 
-	 * @implNote La implementación por defecto proporciona la comprobación de requisitos básicos comunes que, en su mayoría, comprobarían todas las implementaciones de esta interfaz. 
+	 * @return {@code true} si la eliminación fue exitosa. {@code false} si el
+	 *  objeto es {@code null} o no está registrado.
+	 * 
+	 * @implNote La implementación por defecto proporciona la comprobación de 
+	 * requisitos básicos comunes que, en su mayoría, comprobarían todas las 
+	 * implementaciones de esta interfaz. 
 	 */
 	public default boolean delete(T t) {
 		
